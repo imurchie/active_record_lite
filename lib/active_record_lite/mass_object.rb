3932f286977024c0fd94f244c01fbef990dbc76a
Isaac Murchie <imurchie@gmail.com>
2013-10-07 10:10:07 -0700

    Add basic mass assignment checking

--- lib/active_record_lite/mass_object.rb
+++ lib/active_record_lite/mass_object.rb
@@ -1,13 +1,25 @@
 class MassObject
   def self.my_attr_accessible(*attributes)
+    attributes.each do |attribute|
+      attr_accessor attribute
+      self.attributes << attribute.to_s
+    end
   end
 
   def self.attributes
+    @attributes ||= []
   end
 
   def self.parse_all(results)
   end
 
   def initialize(params = {})
+    params.each do |attribute, value|
+      if self.class.attributes.include?(attribute)
+        self.send("#{attribute}=", value)
+      else
+        raise "mass assignment to unregistered attribute #{attribute}"
+      end
+    end
   end
 end
